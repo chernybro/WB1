1adfd37c878c8390708f0d3c52a5c864c6047c2d
chernybro <anvar-1234567@yandex.ru>
2022-05-03 22:21:00 +0300

    Service implemented

--- app/src/main/java/com/chernybro/wb1/MainActivity.kt
+++ app/src/main/java/com/chernybro/wb1/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.chernybro.wb1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.chernybro.wb1.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        binding.apply {
+            btnService.setOnClickListener {
+                startService(Intent(this@MainActivity, ServiceImpl::class.java))
+            }
+            btnProvider.setOnClickListener {
+
+            }
+            btnReceiver.setOnClickListener {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/chernybro/wb1/ServiceImpl.kt
+++ app/src/main/java/com/chernybro/wb1/ServiceImpl.kt
@@ -2,11 +2,65 @@ package com.chernybro.wb1
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+import kotlin.random.Random
+
 
 class ServiceImpl : Service() {
 
+    companion object {
+        private const val TAG = "SERVICE"
+    }
+
+    override fun onCreate() {
+        Log.d(TAG, "Служба создана")
+    }
+
+    private fun startLongProcess() {
+        Handler().post(
+            Runnable { bubbleSort(
+                IntArray(
+                    size = 20000,
+                    init = { Random.nextInt() }
+                ))
+                Toast.makeText(this, "Сервис закончил работу, сортировка закончена", Toast.LENGTH_SHORT)
+                    .show()
+                stopSelf()
+            }
+        )
+    }
+
+    private fun bubbleSort(arr: IntArray): IntArray {
+        var swap = true
+        while (swap) {
+            swap = false
+            for (i in 0 until arr.size - 1) {
+                if (arr[i] > arr[i + 1]) {
+                    val temp = arr[i]
+                    arr[i] = arr[i + 1]
+                    arr[i + 1] = temp
+                    swap = true
+                }
+            }
+        }
+        return arr
+    }
+
     override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "Сервис начал работу", Toast.LENGTH_SHORT)
+            .show()
+        startLongProcess()
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "Служба уничтожена")
     }
 }
