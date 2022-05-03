4d87b193f2ff55727388759b408e56b757a0ffdb
chernybro <anvar-1234567@yandex.ru>
2022-05-03 21:00:23 +0300

    app skeleton implemented

---
+++ app/src/main/java/com/chernybro/wb1/BroadcastReceiverImpl.kt
@@ -0,0 +1,12 @@
+package com.chernybro.wb1
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BroadcastReceiverImpl : BroadcastReceiver() {
+
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/chernybro/wb1/ServiceImpl.kt
@@ -0,0 +1,12 @@
+package com.chernybro.wb1
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ServiceImpl : Service() {
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
